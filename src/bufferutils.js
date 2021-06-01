d89779daf2fc0fc4968bc1b6fd17afdfeff6a072
sasa <sasacoh@gmail.com>
2021-06-01 14:18:24 +0200

    added bigint support

--- src/bufferutils.js
+++ src/bufferutils.js
@@ -22,8 +22,8 @@ function readUInt64LE(buffer, offset) {
 exports.readUInt64LE = readUInt64LE;
 function writeUInt64LE(buffer, value, offset) {
   if (typeof value === 'bigint') {
-    buffer.writeInt32LE(Number(value) & -1, offset);
-    buffer.writeUInt32LE(Number(value) / 0x100000000, offset + 4);
+    buffer.writeUInt32LE(Number(value & 0xFFFFFFFFn), offset);
+    buffer.writeUInt32LE(Number(value >> 32n), offset + 4);
   } else {
     verifuint(value, 0x001fffffffffffff);
     buffer.writeInt32LE(value & -1, offset);
